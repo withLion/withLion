0334941943823603593a946c451e27376ea4db20
HaYoonsoo <ahakilo103@gmail.com>
2022-05-03 22:48:02 +0900

    fix: broken character

--- WithLion/settings.py
+++ WithLion/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$&cjyp$rl-fyma!$b*g01r418o-w$g4$vi%j_a5p$6b&kk6p%a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
